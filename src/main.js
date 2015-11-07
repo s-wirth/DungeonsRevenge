1205d12fa3ee9a3b18384923a3b7634d43a47d31
Will Madden <w.a.madden@gmail.com>
2015-11-07 17:29:36 +0100

    Add a simple proof of concept for keyboard handling

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
-import css from 'main.css';
-new Promise((resolve) => resolve())
-  .then(() => console.log("hello world!"));
+// import css from "main.css";
+/* eslint no-console:0 */
+import Mousetrap from "mousetrap";
+
+let playerPosition = {
+  x: 0,
+  y: 0,
+};
+
+Mousetrap.bind("up", () => {
+  playerPosition.y += 1;
+  console.log("playerPosition =", playerPosition);
+});
+
+Mousetrap.bind("right", () => {
+  playerPosition.x += 1;
+  console.log("playerPosition =", playerPosition);
+});
+
+Mousetrap.bind("down", () => {
+  playerPosition.y -= 1;
+  console.log("playerPosition =", playerPosition);
+});
+
+Mousetrap.bind("left", () => {
+  playerPosition.x -= 1;
+  console.log("playerPosition =", playerPosition);
+});
