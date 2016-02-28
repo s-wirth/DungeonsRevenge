fa554576b6ac57ae2b08fc996bdf26bc1080978b
s-wirth <a.sophiewirth@gmail.com>
2016-02-28 18:19:47 +0100

    Extracted tiles into a separate Dungeon element

--- src/elements/App.js
+++ src/elements/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GameState from "GameState";
+import Dungeon from "elements/Dungeon";
 
 // This has to match the tile width in the CSS
 const TILE_WIDTH = 16;
@@ -22,29 +23,6 @@ const App = React.createClass({
     });
   },
 
-  renderMapTiles() {
-    let map = this.state.map;
-
-    if (!map) return null;
-
-    let width = map.shape[0];
-    let height = map.shape[1];
-    let result = [];
-
-    for (let x = 0; x < width; x++) {
-      for (let y = 0; y < height; y++) {
-        let mapTile = map.get(x, y);
-        if (mapTile) {
-          result.push(
-            <div className={ `${mapTile.type}-tile` } key={`${x}-${y}`} style={{ left: x * TILE_WIDTH, top: y * TILE_WIDTH }} />
-          );
-        }
-      }
-    }
-
-    return result;
-  },
-
   renderCreatures() {
     let creatures = this.state.creatures;
 
@@ -64,13 +42,13 @@ const App = React.createClass({
   },
 
   render() {
-    let playerPosition = this.state.playerPosition;
+    let { playerPosition, map } = this.state;
     let x = playerPosition.x;
     let y = playerPosition.y;
 
     return (
       <div className="scene">
-        { this.renderMapTiles() }
+        <Dungeon map={ map } />
         <div className="player" style={{ left: x * TILE_WIDTH, top: y * TILE_WIDTH }} key="player"> :) </div>
         { this.renderCreatures() }
       </div>
---
+++ src/elements/Dungeon.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// This has to match the tile width in the CSS
+const TILE_WIDTH = 16;
+
+function Dungeon({ map }) {
+  function renderTiles() {
+    if (!map) return null;
+
+    let width = map.shape[0];
+    let height = map.shape[1];
+    let result = [];
+
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        let mapTile = map.get(x, y);
+        if (mapTile) {
+          result.push(
+            <div className={ `${mapTile.type}-tile` } key={`${x}-${y}`}
+                 style={{ left: x * TILE_WIDTH, top: y * TILE_WIDTH }}/>
+          );
+        }
+      }
+    }
+
+    return result;
+  }
+
+  return <div>{ renderTiles() }</div>;
+}
+
+export default Dungeon;
