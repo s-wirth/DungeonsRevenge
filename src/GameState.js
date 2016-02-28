66786ef4286043455f8fcbe817a3115ab40b92ce
s-wirth <a.sophiewirth@gmail.com>
2016-02-28 17:29:36 +0100

    Spawn multiple enemies in random locations in each room

--- src/GameState.js
+++ src/GameState.js
@@ -29,19 +29,40 @@ function makePlayer(x, y) {
 function makeMap() {
   let map = ndarray([], [MAP_WIDTH, MAP_HEIGHT]);
   let rotMap = new ROT.Map.Digger(MAP_WIDTH, MAP_HEIGHT, {roomWidth: [7,12], roomHeight: [7,13], dugPercentage: 0.5});
+  map.rotMap = rotMap;
+
   rotMap.create(function(x, y, wall) {
     map.set(x, y, wall ? makeTile("wall") : null);
   });
-  setStairs(map, rotMap);
+
+  setStairs(map);
+
   return map;
 }
 
-function setStairs(map, rotMap) {
+function setStairs(map) {
+  let rotMap = map.rotMap;
   let rooms = rotMap.getRooms();
   var stairsDownPosition = rooms[0].getCenter();
   map.set(stairsDownPosition[0], stairsDownPosition[1], makeTile("stairsDown"));
   map.set(rooms[rooms.length - 1].getCenter()[0], rooms[rooms.length - 1].getCenter()[1], makeTile("stairsUp"));
   map.stairsDownPosition = stairsDownPosition;
+}
+
+function randomPositionIn(room) {
+  return [
+    Math.floor(room.getLeft() + (room.getRight() - room.getLeft()) * Math.random()),
+    Math.floor(room.getTop() + (room.getBottom() - room.getTop()) * Math.random()),
+  ];
+}
+
+function spawnEnemies(map) {
+  let rotMap = map.rotMap;
+
+  return rotMap.getRooms().map((room) => {
+    let position = randomPositionIn(room);
+    return makeCreature(position[0], position[1], 'enemy');
+  });
 }
 
 export function makeGameState() {
@@ -50,9 +71,7 @@ export function makeGameState() {
   gameState.map = makeMap();
   gameState.playerPosition = makePlayer(gameState.map.stairsDownPosition[0], gameState.map.stairsDownPosition[1]);
 
-  gameState.creatures = [
-    makeCreature(1, 1, 'enemy'),
-  ];
+  gameState.creatures = spawnEnemies(gameState.map);
 
   Object.assign(gameState, {
     updatePlayerPosition(destination) {
