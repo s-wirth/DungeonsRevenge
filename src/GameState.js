8827c7b5d9f2cdbf12d1ac2bbaf58deef76cf6a4
s-wirth <a.sophiewirth@gmail.com>
2016-03-06 19:26:52 +0100

    Extracted level logic into its own file

--- src/GameState.js
+++ src/GameState.js
@@ -1,47 +1,12 @@
 import EventEmitter from "eventemitter2";
 import _ from "lodash";
-import ndarray from "ndarray";
-import ROT from "rot-js";
 import {
   makePlayer,
   spawnEnemies,
   makeCreatureAct,
   makeCreatureAttack
 } from "logic/creatures";
-
-const MAP_WIDTH = 80;
-const MAP_HEIGHT = 40;
-
-function makeTile(type) {
-  return {
-    type,
-  };
-}
-
-
-function makeMap() {
-  let map = ndarray([], [MAP_WIDTH, MAP_HEIGHT]);
-  let rotMap = new ROT.Map.Digger(MAP_WIDTH, MAP_HEIGHT, {roomWidth: [7, 12], roomHeight: [7, 13], dugPercentage: 0.5});
-  map.rotMap = rotMap;
-
-  rotMap.create(function (x, y, wall) {
-    map.set(x, y, wall ? makeTile("wall") : null);
-  });
-
-  setStairs(map);
-
-  return map;
-}
-
-function setStairs(map) {
-  let rotMap = map.rotMap;
-  let rooms = rotMap.getRooms();
-  var stairsDownPosition = rooms[0].getCenter();
-  map.set(stairsDownPosition[0], stairsDownPosition[1], makeTile("stairsDown"));
-  map.set(rooms[rooms.length - 1].getCenter()[0], rooms[rooms.length - 1].getCenter()[1], makeTile("stairsUp"));
-  map.stairsDownPosition = stairsDownPosition;
-}
-
+import {makeMap} from "logic/levels";
 
 export function makeGameState() {
   let gameState = new EventEmitter();
