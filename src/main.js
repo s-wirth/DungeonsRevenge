b48e8739b13708ea8d4bfb5b54904f4fea6f7648
Will Madden <w.a.madden@gmail.com>
2016-03-20 09:34:36 +0100

    Add FastClick
    It removes the delay on mobile between tapping the screen and a click event being emitted (normally 300ms)

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "elements/App";
 import controls from "controls";
+import FastClick from "fastclick";
 /* eslint-enable */
+
+FastClick.attach(document.body);
 
 const rootElement = document.createElement("div");
 rootElement.className = "root";
