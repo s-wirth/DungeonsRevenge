412d3ee4e5d8f276ce10b191bde756192290c385
Will Madden <w.a.madden@gmail.com>
2016-03-26 15:49:24 +0100

    (refactor) Refactor `IntroScreen` using `Screen` and `Scroll` elements
    And make the intro text a bit better

--- src/elements/IntroScreen.js
+++ src/elements/IntroScreen.js
@@ -1,38 +1,59 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import Tween from "gsap";
+import bindFunctions from "util/bindFunctions";
+import Screen from "elements/ui/Screen";
+import Scroll from "elements/ui/Scroll";
 import "css/ScrollScreen";
 
 class IntroScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.onKeyUp = this.onKeyUp.bind(this);
+    bindFunctions(this, ["closeScreen", "stopAutoScrolling"]);
   }
 
   componentDidMount() {
+    const container = this.refs.scroll.refs.content;
     ReactDOM.findDOMNode(this).focus();
+    this.tween = Tween.to(container, 20, {
+      delay: 5,
+      ease: "Linear",
+      scrollTop: container.scrollHeight - container.clientHeight,
+    });
   }
 
-  onKeyUp() {
+  closeScreen() {
     const { switchFromIntroToDungeon } = this.props;
     switchFromIntroToDungeon();
   }
 
+  stopAutoScrolling() {
+    this.tween.kill();
+  }
+
   render() {
     return (
-      <div
-        className="ScrollScreen ScrollScreen--introScreen"
-        tabIndex="99" onKeyUp={ this.onKeyUp } onClick={ this.onKeyUp }
-      >
-        <h1>Dungeon's Revenge</h1>
-        <h3>
-          This is a Rogue Like. To beat it, you have hack your way through the dungeon and defeat
-          the boss.
-        </h3>
-        <b>If you die you're done for. No extra lives.</b>
-        <br />
-        <b>Move with the arrow keys</b>
-        <h2>Press any key to start</h2>
-      </div>
+      <Screen onKeyPress={ this.closeScreen } onClick={ this.closeScreen }>
+        <Scroll
+          ref="scroll"
+          className="Scroll--introScroll"
+          onMouseMove={ this.stopAutoScrolling }
+          onTouchStart={ this.stopAutoScrolling }
+        >
+          <h1>Dungeon's Revenge</h1>
+          <p>
+            You awake. The familiar stench of the dungeon surrounds you. Grime and detritus
+            litter the floor and you hear critters scampering in the shadows.
+          </p>
+          <p>
+            You are a creature of the darkness. Your home has been invaded by
+            fortune-seeking adventurers who slaughtered your kin and stole your treasures.
+          </p>
+          <p>
+            Now it's time for revenge...
+          </p>
+        </Scroll>
+      </Screen>
     );
   }
 }
