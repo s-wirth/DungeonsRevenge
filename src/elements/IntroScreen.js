814e67d57c426b8410d96598150845a910ce341f
Will Madden <w.a.madden@gmail.com>
2016-03-26 16:53:36 +0100

    (feat) Hide the intro screen after scroll completes

--- src/elements/IntroScreen.js
+++ src/elements/IntroScreen.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import Tween from "gsap";
+import "gsap";
 import bindFunctions from "util/bindFunctions";
 import Screen from "elements/ui/Screen";
 import Scroll from "elements/ui/Scroll";
@@ -15,11 +15,18 @@ class IntroScreen extends React.Component {
   componentDidMount() {
     const container = this.refs.scroll.refs.content;
     ReactDOM.findDOMNode(this).focus();
-    this.tween = Tween.to(container, 20, {
-      delay: 5,
-      ease: "Linear",
-      scrollTop: container.scrollHeight - container.clientHeight,
-    });
+    // For some reason the height of the element isn't correct yet, so we do this on the next tick
+    setTimeout(() => {
+      const timeline = new window.TimelineLite();
+      timeline.add(new window.TweenLite(container, 10, {
+        delay: 5,
+        ease: "Linear",
+        scrollTop: container.scrollHeight - container.clientHeight,
+      }));
+      timeline.add(this.closeScreen, "+=5");
+      this.tween = timeline;
+      timeline.play();
+    }, 0);
   }
 
   closeScreen() {
