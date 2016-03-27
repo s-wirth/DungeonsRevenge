3522799c6363ea26f987d1fc4c60c739d6a8748a
Will Madden <w.a.madden@gmail.com>
2016-03-27 18:01:04 +0200

    (refactor) Convert `SewerLevel` to a stamp

--- src/logic/levels.js
+++ src/logic/levels.js
@@ -1,6 +1,14 @@
 import SewerLevel from "logic/levels/SewerLevel";
 import PestControlLevel from "logic/levels/PestControlLevel";
 
+const LEVELS = {
+  0: (id) => SewerLevel({ id }),
+  1: (id) => SewerLevel({ id }),
+  2: (id) => SewerLevel({ id }),
+  3: (id) => SewerLevel({ id }),
+  4: PestControlLevel,
+};
+
 export function enterPreviousLevel(currentPlayerLevel) {
   return currentPlayerLevel.previousLevel;
 }
--- src/logic/levels/SewerLevel.js
+++ src/logic/levels/SewerLevel.js
@@ -6,6 +6,7 @@ import {
 import {
   makeCreature,
 } from "logic/creatures";
+import stampit from "stampit";
 
 const MAP_WIDTH = 80;
 const MAP_HEIGHT = 40;
@@ -24,59 +25,65 @@ export function spawnEnemies(rooms) {
   });
 }
 
-export default function SewerLevel(id) {
-  const map = Map.create({ id, width: MAP_WIDTH, height: MAP_HEIGHT });
+const SewerLevel = Map.compose(stampit({
+  props: {
+    width: MAP_WIDTH,
+    height: MAP_HEIGHT,
+  },
 
-  function makeDoors(x, y) {
-    map.setTile("door", { x, y });
-  }
-
-  function getDoors(rooms) {
-    for (let i = 0; i < rooms.length; i++) {
-      const room = rooms[i];
-      room.getDoors(makeDoors);
+  init({ instance: map }) {
+    function makeDoors(x, y) {
+      map.setTile("door", { x, y });
     }
-  }
 
-  function healingItemsOnLevel(rooms) {
-    const amount = Math.floor(Math.random() * 2) + 1;
-    const potions = [];
-    for (let i = 0; i <= amount; i++) {
-      const randomRoom = Math.floor(Math.random() * rooms.length);
-      const position = randomPositionIn(rooms[randomRoom]);
+    function getDoors(rooms) {
+      for (let i = 0; i < rooms.length; i++) {
+        const room = rooms[i];
+        room.getDoors(makeDoors);
+      }
+    }
+
+    function healingItemsOnLevel(rooms) {
+      const amount = Math.floor(Math.random() * 2) + 1;
+      const potions = [];
+      for (let i = 0; i <= amount; i++) {
+        const randomRoom = Math.floor(Math.random() * rooms.length);
+        const position = randomPositionIn(rooms[randomRoom]);
+        potions.push(makeItem("healingPotion", { x: position[0], y: position[1] }));
+      }
+      const position = randomPositionIn(rooms[0]);
       potions.push(makeItem("healingPotion", { x: position[0], y: position[1] }));
+      map.items = potions;
     }
-    const position = randomPositionIn(rooms[0]);
-    potions.push(makeItem("healingPotion", { x: position[0], y: position[1] }));
-    map.items = potions;
-  }
 
-  const rotMap = new ROT.Map.Digger(MAP_WIDTH, MAP_HEIGHT, {
-    roomWidth: [7, 12],
-    roomHeight: [7, 13],
-    dugPercentage: 0.5,
-  });
+    const rotMap = new ROT.Map.Digger(MAP_WIDTH, MAP_HEIGHT, {
+      roomWidth: [7, 12],
+      roomHeight: [7, 13],
+      dugPercentage: 0.5,
+    });
 
-  rotMap.create((x, y, wall) => {
-    if (wall) {
-      map.setTile("wall", { x, y });
+    rotMap.create((x, y, wall) => {
+      if (wall) {
+        map.setTile("wall", { x, y });
+      } else {
+        map.setTile("floor", { x, y });
+      }
+    });
+
+    const rooms = rotMap.getRooms();
+
+    if (map.id !== 0) {
+      map.setStairs(rooms[0].getCenter(), rooms[rooms.length - 1].getCenter());
     } else {
-      map.setTile("floor", { x, y });
+      map.setStairs(null, rooms[0].getCenter());
     }
-  });
 
-  const rooms = rotMap.getRooms();
+    map.setInitialPlayerPosition(rooms[0].getCenter()[0], rooms[0].getCenter()[1]);
+    getDoors(rooms);
+    healingItemsOnLevel(rooms);
 
-  if (map.id !== 0) {
-    map.setStairs(rooms[0].getCenter(), rooms[rooms.length - 1].getCenter());
-  } else {
-    map.setStairs(null, rooms[rooms.length - 1].getCenter());
-  }
+    map.creatures = spawnEnemies(rooms);
+  },
+}));
 
-  map.setInitialPlayerPosition(rooms[0].getCenter()[0], rooms[0].getCenter()[1]);
-  getDoors(rooms);
-  healingItemsOnLevel(rooms);
-
-  map.creatures = spawnEnemies(rooms);
-  return map;
-}
+export default SewerLevel;
