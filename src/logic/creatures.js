17e9d3401594febc4e32c4d40a151932842604b0
Will Madden <w.a.madden@gmail.com>
2016-03-28 11:45:10 +0200

    (refactor) Extract common `Creature` base stamp

--- src/logic/creatures.js
+++ src/logic/creatures.js
@@ -1,94 +1,84 @@
 import stampit from "stampit";
+import NoThis from "util/stamps/NoThis";
+import UniqueId from "util/stamps/UniqueId";
 import findPath from "logic/findPath";
 import hashFromList from "util/hashFromList";
 
-let creatureIdCounter = 0;
 const MAX_INVENTORY_SIZE = 9;
 
+const Creature = stampit.compose(NoThis, UniqueId, stampit({
+  props: {
+    inventorySize: MAX_INVENTORY_SIZE,
+  },
+
+  methods: {
+    increaseHealth(creature, amount) {
+      creature.health += amount;
+      if (creature.health > creature.maxHealth) {
+        creature.health = creature.maxHealth;
+      }
+    },
+
+    removeFromInventory(creature, item) {
+      creature.inventory.splice(creature.inventory.indexOf(item), 1);
+    },
+
+    addToInventory(creature, item) {
+      creature.inventory.push(item);
+    },
+  },
+
+  init({ instance }) {
+    Object.assign(instance, {
+      health: instance.maxHealth,
+      inventory: [],
+    });
+  },
+}));
 
 const CREATURE_TYPES = [
-  stampit({
-    props: {
-      type: "player",
-      damage: 3,
-      maxHealth: 0,
-      sightRadius: 5,
-      experience: 0,
-    },
+  Creature.props({
+    type: "player",
+    damage: 3,
+    maxHealth: 0,
+    sightRadius: 5,
+    experience: 0,
   }),
-  stampit({
-    props: {
-      type: "mutantRat",
-      typeName: "Mutant Rat",
-      experienceLootOnKill: 2.78,
-      maxHealth: 2,
-      damage: 0.68,
-      sightRadius: 8,
-      description: `Rats that are abnormally big and agressive, due to the magical waste flushed
-        down the sewer.`,
-    },
+  Creature.props({
+    type: "mutantRat",
+    typeName: "Mutant Rat",
+    experienceLootOnKill: 2.78,
+    maxHealth: 2,
+    damage: 0.68,
+    sightRadius: 8,
+    description: `Rats that are abnormally big and agressive, due to the magical waste flushed down
+      the sewer.`,
   }),
-  stampit({
-    props: {
-      type: "minion",
-      typeName: "Minion",
-      experienceLootOnKill: 5.52,
-      maxHealth: 3.12,
-      damage: 2.50,
-      sightRadius: 8,
-      description: "Footsoldiers from the surface sent to control the denizens of the dungeon.",
-    },
+  Creature.props({
+    type: "minion",
+    typeName: "Minion",
+    experienceLootOnKill: 5.52,
+    maxHealth: 3.12,
+    damage: 2.50,
+    sightRadius: 8,
+    description: "Footsoldiers from the surface sent to control the denizens of the dungeon.",
   }),
-  stampit({
-    props: {
-      type: "pestcontrol",
-      typeName: "Pest Control",
-      experienceLootOnKill: 100,
-      maxHealth: 24.19,
-      damage: 8.98,
-      sightRadius: 8,
-      description: "The Pest Control is a huge man in armor with a morning star.",
-    },
+  Creature.props({
+    type: "pestcontrol",
+    typeName: "Pest Control",
+    experienceLootOnKill: 100,
+    maxHealth: 24.19,
+    damage: 8.98,
+    sightRadius: 8,
+    description: "The Pest Control is a huge man in armor with a morning star.",
   }),
 ];
 
 const CREATURE_TYPES_HASH = hashFromList(CREATURE_TYPES, (stamp) => stamp.fixed.props.type);
 
 export function makeCreature(type, { x, y }) {
-  const id = creatureIdCounter += 1;
   const creatureType = CREATURE_TYPES_HASH[type];
-
-  const creature = creatureType({
-    id,
-    type,
-    x, y,
-    health: creatureType.fixed.props.maxHealth,
-    inventory: [],
-    inventorySize: MAX_INVENTORY_SIZE,
-  });
-
-  function increaseHealth(amount) {
-    creature.health += amount;
-    if (creature.health > creature.maxHealth) {
-      creature.health = creature.maxHealth;
-    }
-  }
-
-  function removeFromInventory(item) {
-    creature.inventory.splice(creature.inventory.indexOf(item), 1);
-  }
-
-  function addToInventory(item) {
-    creature.inventory.push(item);
-  }
-
-  Object.assign(creature, {
-    increaseHealth,
-    removeFromInventory,
-    addToInventory,
-  });
-
-  return creature;
+  return creatureType({ x, y });
 }
 
 function distanceBetween({ x: x1, y: y1 }, { x: x2, y: y2 }) {
